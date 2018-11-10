fb50cd5969cdb95458fd63b04fa6d68a3767d2ae
Aditya Bhatia <bhatia.aditya@gmail.com>
2018-11-10 14:28:06 -0500

    Finished movie data

--- Argumentative Synthesis Project/Argumentative Synthesis Project/Movie.Designer.vb
+++ Argumentative Synthesis Project/Argumentative Synthesis Project/Movie.Designer.vb
@@ -22,6 +22,7 @@ Partial Class frmMovie
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(frmMovie))
         Me.MenuStrip1 = New System.Windows.Forms.MenuStrip()
         Me.ExitToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
         Me.NextToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
@@ -35,30 +36,31 @@ Partial Class frmMovie
         Me.MenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.ExitToolStripMenuItem, Me.NextToolStripMenuItem})
         Me.MenuStrip1.Location = New System.Drawing.Point(0, 0)
         Me.MenuStrip1.Name = "MenuStrip1"
-        Me.MenuStrip1.Size = New System.Drawing.Size(800, 42)
+        Me.MenuStrip1.Size = New System.Drawing.Size(800, 40)
         Me.MenuStrip1.TabIndex = 0
         Me.MenuStrip1.Text = "MenuStrip1"
         '
         'ExitToolStripMenuItem
         '
         Me.ExitToolStripMenuItem.Name = "ExitToolStripMenuItem"
-        Me.ExitToolStripMenuItem.Size = New System.Drawing.Size(64, 38)
+        Me.ExitToolStripMenuItem.Size = New System.Drawing.Size(64, 36)
         Me.ExitToolStripMenuItem.Text = "E&xit"
         '
         'NextToolStripMenuItem
         '
         Me.NextToolStripMenuItem.Name = "NextToolStripMenuItem"
-        Me.NextToolStripMenuItem.Size = New System.Drawing.Size(77, 38)
+        Me.NextToolStripMenuItem.Size = New System.Drawing.Size(77, 36)
         Me.NextToolStripMenuItem.Text = "&Next"
         '
         'lblMovieInfo
         '
+        Me.lblMovieInfo.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.875!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.lblMovieInfo.Location = New System.Drawing.Point(12, 40)
         Me.lblMovieInfo.Name = "lblMovieInfo"
         Me.lblMovieInfo.Size = New System.Drawing.Size(776, 401)
         Me.lblMovieInfo.TabIndex = 1
-        Me.lblMovieInfo.Text = "One of the biggest issues I often see in war is _________________________________" &
-    "_____________________"
+        Me.lblMovieInfo.Text = resources.GetString("lblMovieInfo.Text")
+        Me.lblMovieInfo.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
         '
         'frmMovie
         '
