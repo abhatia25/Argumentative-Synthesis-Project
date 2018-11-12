268730e9e3eabcdf705e6d1e8450cb38885714a6
Aditya Bhatia <bhatia.aditya@gmail.com>
2018-11-12 11:28:41 -0500

    Finished art analysis

--- Argumentative Synthesis Project/Argumentative Synthesis Project/Art.Designer.vb
+++ Argumentative Synthesis Project/Argumentative Synthesis Project/Art.Designer.vb
@@ -22,6 +22,7 @@ Partial Class frmArt
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(frmArt))
         Me.MenuStrip1 = New System.Windows.Forms.MenuStrip()
         Me.ExitToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
         Me.NextToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
@@ -35,41 +36,42 @@ Partial Class frmArt
         Me.MenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.ExitToolStripMenuItem, Me.NextToolStripMenuItem})
         Me.MenuStrip1.Location = New System.Drawing.Point(0, 0)
         Me.MenuStrip1.Name = "MenuStrip1"
-        Me.MenuStrip1.Padding = New System.Windows.Forms.Padding(3, 1, 0, 1)
-        Me.MenuStrip1.Size = New System.Drawing.Size(722, 24)
+        Me.MenuStrip1.Size = New System.Drawing.Size(1444, 40)
         Me.MenuStrip1.TabIndex = 0
         Me.MenuStrip1.Text = "MenuStrip1"
         '
         'ExitToolStripMenuItem
         '
         Me.ExitToolStripMenuItem.Name = "ExitToolStripMenuItem"
-        Me.ExitToolStripMenuItem.Size = New System.Drawing.Size(37, 22)
+        Me.ExitToolStripMenuItem.Size = New System.Drawing.Size(64, 36)
         Me.ExitToolStripMenuItem.Text = "E&xit"
         '
         'NextToolStripMenuItem
         '
         Me.NextToolStripMenuItem.Name = "NextToolStripMenuItem"
-        Me.NextToolStripMenuItem.Size = New System.Drawing.Size(43, 22)
+        Me.NextToolStripMenuItem.Size = New System.Drawing.Size(77, 36)
         Me.NextToolStripMenuItem.Text = "&Next"
         '
         'lblArtData
         '
-        Me.lblArtData.Location = New System.Drawing.Point(12, 17)
+        Me.lblArtData.Font = New System.Drawing.Font("Microsoft Sans Serif", 24.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblArtData.Location = New System.Drawing.Point(24, 33)
+        Me.lblArtData.Margin = New System.Windows.Forms.Padding(6, 0, 6, 0)
         Me.lblArtData.Name = "lblArtData"
-        Me.lblArtData.Size = New System.Drawing.Size(698, 417)
+        Me.lblArtData.Size = New System.Drawing.Size(1396, 802)
         Me.lblArtData.TabIndex = 2
-        Me.lblArtData.Text = "Label1"
+        Me.lblArtData.Text = resources.GetString("lblArtData.Text")
         Me.lblArtData.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
         '
         'frmArt
         '
-        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(12.0!, 25.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(722, 450)
+        Me.ClientSize = New System.Drawing.Size(1444, 865)
         Me.Controls.Add(Me.lblArtData)
         Me.Controls.Add(Me.MenuStrip1)
         Me.MainMenuStrip = Me.MenuStrip1
-        Me.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
+        Me.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
         Me.Name = "frmArt"
         Me.Text = "Art"
         Me.MenuStrip1.ResumeLayout(False)
--- Argumentative Synthesis Project/Argumentative Synthesis Project/Movie.Designer.vb
+++ Argumentative Synthesis Project/Argumentative Synthesis Project/Movie.Designer.vb
@@ -36,43 +36,42 @@ Partial Class frmMovie
         Me.MenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.ExitToolStripMenuItem, Me.NextToolStripMenuItem})
         Me.MenuStrip1.Location = New System.Drawing.Point(0, 0)
         Me.MenuStrip1.Name = "MenuStrip1"
-        Me.MenuStrip1.Padding = New System.Windows.Forms.Padding(3, 1, 0, 1)
-        Me.MenuStrip1.Size = New System.Drawing.Size(722, 24)
+        Me.MenuStrip1.Size = New System.Drawing.Size(1444, 40)
         Me.MenuStrip1.TabIndex = 0
         Me.MenuStrip1.Text = "MenuStrip1"
         '
         'ExitToolStripMenuItem
         '
         Me.ExitToolStripMenuItem.Name = "ExitToolStripMenuItem"
-        Me.ExitToolStripMenuItem.Size = New System.Drawing.Size(37, 22)
+        Me.ExitToolStripMenuItem.Size = New System.Drawing.Size(64, 36)
         Me.ExitToolStripMenuItem.Text = "E&xit"
         '
         'NextToolStripMenuItem
         '
         Me.NextToolStripMenuItem.Name = "NextToolStripMenuItem"
-        Me.NextToolStripMenuItem.Size = New System.Drawing.Size(43, 22)
+        Me.NextToolStripMenuItem.Size = New System.Drawing.Size(77, 36)
         Me.NextToolStripMenuItem.Text = "&Next"
         '
         'lblMovieInfo
         '
         Me.lblMovieInfo.Font = New System.Drawing.Font("Microsoft Sans Serif", 20.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.lblMovieInfo.Location = New System.Drawing.Point(6, 21)
-        Me.lblMovieInfo.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
+        Me.lblMovieInfo.Location = New System.Drawing.Point(12, 40)
+        Me.lblMovieInfo.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
         Me.lblMovieInfo.Name = "lblMovieInfo"
-        Me.lblMovieInfo.Size = New System.Drawing.Size(705, 420)
+        Me.lblMovieInfo.Size = New System.Drawing.Size(1410, 808)
         Me.lblMovieInfo.TabIndex = 1
         Me.lblMovieInfo.Text = resources.GetString("lblMovieInfo.Text")
         Me.lblMovieInfo.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
         '
         'frmMovie
         '
-        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(12.0!, 25.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(722, 450)
+        Me.ClientSize = New System.Drawing.Size(1444, 865)
         Me.Controls.Add(Me.lblMovieInfo)
         Me.Controls.Add(Me.MenuStrip1)
         Me.MainMenuStrip = Me.MenuStrip1
-        Me.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
+        Me.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
         Me.Name = "frmMovie"
         Me.Text = "Movie"
         Me.MenuStrip1.ResumeLayout(False)
